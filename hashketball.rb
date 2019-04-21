cc40020068ca8a2fd2c2e910f11928c980cbed45
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-21 06:53:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,7 +186,17 @@ def big_shoe_rebounds
 end
 
 def most_points_scored
-  
+  most_point = 0
+   player = nil
+   game_hash.each do |location, team_data|
+      team_data[:players].each do |player_name, value|
+         if value[:points] > most_point
+            most_point = value[:points]
+            player = player_name
+         end
+     end
+   end
+   player
 end
 
 def winning_team
